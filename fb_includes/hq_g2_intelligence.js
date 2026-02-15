d51bc58eb130e7df9a46fc32ae43c4631b87993b
fishbotdev <fishbotdev@gmail.com>
2026-02-15 12:44:38 +1100

    Updated license to GPL2.0+

--- fb_includes/hq_g2_intelligence.js
+++ fb_includes/hq_g2_intelligence.js
@@ -1,18 +1,19 @@
 /*
 	This file is part of FishBot, a Warzone 2100 AI.
 
-	FishBot is free software: you can redistribute it and/or modify it under the terms of the 
-	GNU General Public License as published by the Free Software Foundation, either version 3 
-	of the License, or (at your option) any later version.
+	FishBot is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-	FishBot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-	See the GNU General Public License for more details.
+	FishBot is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License along with this program. 
-	If not, see <https://www.gnu.org/licenses/> or <https://www.gnu.org/licenses/gpl-3.0.html>.
+	If not, see <https://www.gnu.org/licenses/>.
 */
-
 class armyIntelligence {
 
 	constructor() {
