104f553e6fd2bedadbb48c5a5c939be7daaecc39
fishbotdev <fishbotdeveloper@gmail.com>
2026-03-12 14:07:22 +1100

    - updated test scripts

--- process_results/process_headless_autogame_results.py
+++ process_results/process_headless_autogame_results.py
@@ -227,8 +227,9 @@ def show_hard_cobra_results():
     # test_1 = getcwd() + rf"\process_results/v4/" + "83ebcd1,hard,cobra,50g" + ".txt"              # v4 dev: 4.6.3, [34 / 50 wins] after first intel optimisation
     # test_1 = getcwd() + rf"\process_results/v4/" + "fd4692d,hard,cobra,50g" + ".txt"              # v4 dev: 4.6.3, [36 / 50 wins] first new scheduler implementation
     # test_1 = getcwd() + rf"\process_results/v4/" + "603cca0,hard,cobra,50g" + ".txt"              # v4 dev: 4.6.3, [34 / 50 wins] after full migration of existing functions to the new scheduler
-    test_1 = getcwd() + rf"\process_results/v4/" + "fbf5655,hard,cobra,50g" + ".txt"              # v4 dev: 4.6.3, [35 / 50 wins] reduced COP integration from 1Hz to 1/2Hz
-    test_2 = getcwd() + rf"\process_results/v4/" + "e0f8c66,hard,cobra,100g" + ".txt"              # v4 dev: 4.6.3, [57 / 99 = 56% wr]; thinking about releasing v3.1.0 with performance improvements
+    # test_1 = getcwd() + rf"\process_results/v4/" + "fbf5655,hard,cobra,50g" + ".txt"              # v4 dev: 4.6.3, [35 / 50 wins] reduced COP integration from 1Hz to 1/2Hz
+    test_1 = getcwd() + rf"\process_results/v4/" + "e0f8c66,hard,cobra,100g" + ".txt"              # v4 dev: 4.6.3, [57 / 99 = 56% wr]; thinking about releasing v3.1.0 with performance improvements
+    test_2 = getcwd() + rf"\process_results/v4/" + "29bb952,hard,cobra,100g" + ".txt"              # v4 dev: 4.6.3, [64 / 100 = 64% wr]; small AI behaviour improvements
 
     commit1 = test_1.split(rf"v4/")[1].split(",")[0]
     commit2 = test_2.split(rf"v4/")[1].split(",")[0]
