9f768030533062b6b14c05e6a66221bc2ae08086
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-03-20 12:16:37 +1100

    added test script to run autogames from prod folder

---
+++ run_wz2100_headless_autogame_PRODCONFIG.bat
@@ -0,0 +1,20 @@
+:: I cloned fishbot into a new configuration directory ('PRODCONFIG') & am running autogames from this 'production' folder.
+:: This means I can run autogames & perform development simultaneously using the same WZ2100 .exe, e.g. 
+:: different config directories for dev / prod = different mods directories = different fishbot instances
+
+:: PRODCONFIG is a clone of the devconfig folder.
+:: This means that changes to the development config directory (e.g. new items in the 'tests' folder) can be pulled from VC
+
+for /L %%i in (1,1,100) do (
+	echo iteration %%i
+	"Warzone 2100\bin\warzone2100.exe" --configdir="Warzone 2100\PRODCONFIG" --skirmish="GAMMA_MEDIUM_COBRA_T2.json" --enableconsole --headless --autogame
+)
+
+:: json template:
+:: https://github.com/Warzone2100/warzone2100/blob/ebeaaa7958f35879eea7b57474eff0c89aa4fb03/data/mp/tests/highground.json
+
+:: command line parameters:
+:: https://github.com/Warzone2100/warzone2100/blob/ebeaaa7958f35879eea7b57474eff0c89aa4fb03/src/clparse.cpp#L21
+
+pause
+
