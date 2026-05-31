e01727f882011a27bea0909cf7231a307a624e19
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-05-31 17:24:16 +1000

    implemented BFS with diagonal offsets

--- python_helper_scripts/map_analysis/pathfinding_test.py
+++ python_helper_scripts/map_analysis/pathfinding_test.py
@@ -109,6 +109,91 @@ def mock_find_path(map, start: tuple, goal: tuple) -> list[tuple]:
 
 
 def find_path_bfs(map, start: tuple, goal: tuple) -> list[tuple]:
+    """
+    References
+    1. DFS/BFS - Abdul Bari
+    2. Breadth First Search Algorithm | Shortest Path | Graph Theory - WilliamFiset
+    """
+
+    queue = [start]
+    visited = [start]
+    parent_indexes = [None]
+
+    result = [goal]
+
+    def get_valid_neighbours(pos, map):
+        ymax, xmax = map.shape
+
+        valid_neighbours = []
+        offsets = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1,-1), (-1, 1), (1, 1), (1,-1)]
+
+        tx = 0  # initialise these variables so they are reused
+        ty = 0
+
+        # Test for valid locations
+        for offset in offsets:
+            new_pos = (pos[0] + offset[0], pos[1] + offset[1])
+            # print(f"new pos: {new_pos}")
+
+            # Test in map bounds
+            tx = new_pos[0]
+            ty = new_pos[1]
+
+            if tx < 0 or tx >= xmax:
+                continue
+            if ty < 0 or ty >= ymax:
+                continue
+
+            valid_tile = map[ty][tx]    # NOTE: array indexing map needs to be in this order because it looks up 'rows' = 'y', and then 'cols' = 'x'
+            if valid_tile:
+                valid_neighbours.append(new_pos)
+
+        return valid_neighbours
+
+    iters = 0
+    max_iterations = 10000      # to prevent the algorithm from running forever if I make a mistake
+    while len(queue) > 0 and iters < max_iterations:
+        node = queue.pop(0)
+        parent_index = visited.index(node) if (node in visited) else None       # never returns `None` since all parents are registered in 'visited' before the child is spawned
+
+        neighbours = get_valid_neighbours(node, map)
+        for neighbour in neighbours:
+            if neighbour in visited:
+                continue
+
+            queue.append(neighbour)
+
+            visited.append(neighbour)       # add the new neighbour so it's not reused in future iterations
+            parent_indexes.append(parent_index)       # add the parent node of the newest visited node (used in path traceback)
+
+        iters += 1
+
+    # print(f"visited: {visited}")
+    # print(f"parent: {parent_indexes}")
+    print(f"BFS completed in {iters} iterations.")
+
+    # back out the path, starting from the end node
+    idx = visited.index(goal) if (goal in visited) else None
+    print(f'goal index in visited: {idx}')
+
+    iters = 0
+    while idx != None and iters < max_iterations:
+        idx = parent_indexes[idx]
+
+        if idx is not None:
+            # Append parent node information to result
+            p = visited[idx]
+            result.append(p)
+            print(f"\t - parent: {p}")
+
+        iters += 1
+
+    if idx is None:
+        result.reverse()        # start to finish
+
+        # print(f"result: {result}")
+        return result
+
     return []
 
 
@@ -163,14 +248,14 @@ if __name__ == '__main__':
 
     ################### USER CONFIG START ###################
     START = (10, 15)
-    GOAL = (50, 40)
+    GOAL = (70, 70)
 
     FILE_NAME = "gamma_terrainType"     # this is the data obtained from the `MapTiles.terrainType` global
     ################### USER CONFIG END ###################
 
     test_harness = BasicPathTest()
 
-    if True:    # for development, overwrites all user parameters with those from the `test_harness`
+    if False:    # for development, overwrites all user parameters with those from the `test_harness`
         START = test_harness.start
         GOAL = test_harness.goal
         passability_map = test_harness.test_map
