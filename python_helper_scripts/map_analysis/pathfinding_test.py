4dd920f3801e6c9e3fc805f9275a5acc078ad9d1
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-06-19 08:14:09 +1000

    refactor: added custom minheap to reduce function overhead

--- python_helper_scripts/map_analysis/pathfinding_test.py
+++ python_helper_scripts/map_analysis/pathfinding_test.py
@@ -40,11 +40,12 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 from time import perf_counter as get_time
-from typing import List, Callable, Any
+from typing import List, Callable, Any, Optional
 import line_profiler        # pip install line_profiler
+from math import floor
 
 # Disclaimer: most of the helper functions are implemented by AI (ChatGPT), but
-# all of the `find_path_[x]` functions are implemented by hand for learning.
+# all of the `find_path_[x]` functions & the binary heap are implemented by hand for learning.
 # (This may explain some of the residual parallel redundant logic)
 
 ############################## HELPER FUNCTIONS ##############################
@@ -326,8 +327,199 @@ def find_path_greedy_best_first_search(map, start: tuple, goal: tuple) -> list[t
     return []
 
 
-from binary_heap import MyBinaryMinHeap as BinaryHeap
+class MyAStarMinHeap:
+    # This heap is implemented zero-index formulas
+    #   Node i (zero indexed)
+    #   Left child `2*i+1`
+    #   Right child `2*i+2`
+    #   Parent node `floor((i - 1) / 2)`
 
+    def __init__(self):
+        self.heap: list[float | int] = []
+
+    def __len__(self) -> int:
+        return len(self.heap)
+
+    def print_heap(self) -> None:
+        # Function 1: Visualisation is important for learning & debugging, so I build this first
+        # The implementation is not very clean but it does the job.
+
+        print(f"\n{self.heap} represented as:\n")
+        curr_heap_index = 0
+        curr_upper_bound = 0
+        HEAP_LEN = len(self.heap)
+
+        all_level_entries = []
+
+        for i in range(10):       # max displayable depth
+            curr_upper_bound += 2 ** i
+
+            entries_on_level = []
+            while curr_heap_index < min(curr_upper_bound, HEAP_LEN):
+                entries_on_level.append(f'{self.heap[curr_heap_index]:2d}')
+                curr_heap_index += 1
+
+            # print('  '.join(entries_on_level))
+
+            all_level_entries.append(entries_on_level)
+
+            if curr_upper_bound > HEAP_LEN:
+                break
+        else:
+            raise ValueError("TREE IS GREATER THAN 10 LEVELS DEEP")
+
+        # print(len(all_level_entries))
+
+        print_rows = []
+
+        # Build the tree from the bottom
+        NUM_ENTRIES = 2 ** (len(all_level_entries) - 1)
+        last_row_entries = ['__' for _ in range(NUM_ENTRIES)]
+
+        # build the last row, including blanks where there are no entries
+        for i, entry in enumerate(all_level_entries[-1]):
+            last_row_entries[i] = entry
+
+        # add spaces in between each entry to form the final row
+        NUM_SPACES = (NUM_ENTRIES - 1)
+        last_row = []
+        for i in range(NUM_SPACES):
+            last_row.append(last_row_entries[i])
+            last_row.append('  ')
+        last_row.append(last_row_entries[-1])
+
+        NUM_SLOTS = NUM_ENTRIES + NUM_SPACES        # these is the slots for printing
+
+        # print(''.join(last_row))
+
+        print_rows.append(last_row)
+
+        # After manually constructing the last row, algorithmically generate the rest of the spacing / rows
+        filled_indexes = [2*i for i in range(NUM_ENTRIES)]
+
+        for i in range(len(all_level_entries)-2, -1, -1):
+            # Calculate positions for numbers to live
+            temp_filled_indexes = []
+            while len(filled_indexes) > 0:
+                last_entry = filled_indexes.pop()
+                second_last_entry = filled_indexes.pop()
+
+                temp_filled_indexes.append(int((last_entry+second_last_entry)/2))
+
+            temp_filled_indexes.reverse()
+
+            # print(temp_filled_indexes)
+            filled_indexes = temp_filled_indexes
+
+            # Use the new filled indexes to insert entries
+            curr_row_entries = all_level_entries[i]
+            curr_print_row = ['  ' for _ in range(NUM_SLOTS)]
+            for entry_idx, print_idx in enumerate(filled_indexes):
+                curr_print_row[print_idx] = curr_row_entries[entry_idx]
+
+            print_rows.append(curr_print_row)
+
+        print_rows.reverse()
+
+        for row in print_rows:
+            print(''.join(row))
+
+
+    def push(self, value) -> None:
+        self.heap.append(value)     # this adds it to the end of the complete binary tree
+
+        # Bubble up the tree to make the heap-invariant true
+        # Algorithm:
+        #   1. Compare with parent.
+        #   2. If smaller, swap, else, do nothing.
+        #   3. Recursively perform until curr_idx is at the root (index 0) or the parent is smaller
+
+        curr_idx = len(self.heap) - 1
+
+        while curr_idx > 0:
+            parent_idx = floor((curr_idx - 1) / 2)
+
+            curr_value = self.heap[curr_idx]
+            parent_value = self.heap[parent_idx]
+
+            if parent_value['f'] < curr_value['f']:  # normal minheap invariant for f
+                break
+            elif parent_value['f'] == curr_value['f'] and parent_value['h'] <= curr_value['h']:  # A-star prioritisation of nodes closer to the target
+                break
+
+            self.heap[parent_idx] = curr_value
+            self.heap[curr_idx] = parent_value
+            curr_idx = parent_idx
+
+    def peek(self) -> Optional:
+        return self.heap[0] if len(self.heap) > 0 else None
+
+    def pop(self) -> Optional:
+        if len(self.heap) == 0:
+            return None
+
+        last_entry = self.heap.pop()
+        if len(self.heap) == 0:
+            return last_entry
+
+        first_entry = self.heap[0]
+        self.heap[0] = last_entry
+
+        # Bubble down to preserve the heap invariant
+        # Algorithm:
+        #   1. Find the min of the two children nodes
+        #   2. Compare the parent to the minimum of the two children nodes
+        #   3. If child is smaller, swap, else, break.
+        curr_idx = 0
+        LAST_IDX = len(self.heap) - 1
+
+        while curr_idx <= LAST_IDX:
+            curr_value = self.heap[curr_idx]
+
+            child1_idx = 2 * curr_idx + 1
+            child2_idx = 2 * curr_idx + 2
+
+            child1 = None
+            child2 = None
+            if child1_idx <= LAST_IDX:
+                child1_value = self.heap[child1_idx]
+                child1 = (child1_idx, child1_value)
+
+            if child2_idx <= LAST_IDX:
+                child2_value = self.heap[child2_idx]
+                child2 = (child2_idx, child2_value)
+
+            if child1 is None and child2 is None:
+                break
+
+            if child1 is not None and child2 is None:
+                child_idx, child_value = child1
+            elif child1 is None and child2 is not None:
+                child_idx, child_value = child2
+            else:
+                if child1_value['f'] < child2_value['f']:  # normal minheap invariant for f
+                    child_idx, child_value = child1
+                elif child1_value['f'] == child2_value['f'] and child1_value['h'] <= child2_value['h']: # A-star prioritisation of nodes closer to the target
+                    child_idx, child_value = child1
+                else:
+                    child_idx, child_value = child2
+
+            if curr_value['f'] < child_value['f']:  # normal minheap invariant for f
+                break
+            elif curr_value['f'] == child_value['f'] and curr_value['h'] <= child_value['h']:  # A-star prioritisation of nodes closer to the target
+                break
+
+            # Else swap & continue to recurse
+            self.heap[curr_idx] = child_value
+            self.heap[child_idx] = curr_value
+            curr_idx = child_idx
+
+        return first_entry
+
+
+    def create_heap(self, values: list[float | int]) -> None:
+        for v in values:
+            self.push(v)
 
 # kernprof -l pathfinding_test.py
 # python -m line_profiler pathfinding_test.py.lprof
@@ -372,32 +564,6 @@ def find_path_astar(map, start: tuple, goal: tuple) -> list[tuple]:
 
         return False
 
-    def satisfies_fcost_invariant(parent_value, child_value) -> bool:
-        if parent_value['f'] < child_value['f']:        # normal minheap invariant for f
-            return True
-        elif parent_value['f'] == child_value['f']:
-            if parent_value['h'] <= child_value['h']:    # A-star prioritisation of nodes closer to the target
-                return True
-            else:
-                return False
-        else:
-            return False
-
-    def get_lowest_fcost_child(child1: tuple, child2: tuple) -> tuple:
-
-        child1_idx, child1_node = child1
-        child2_idx, child2_node = child2
-
-        if child1_node['f'] < child2_node['f']:        # normal minheap invariant for f
-            return child1
-        elif child1_node['f'] == child2_node['f']:
-            if child1_node['h'] <= child2_node['h']:    # A-star prioritisation of nodes closer to the target
-                return child1
-            else:
-                return child2
-        else:
-            return child2
-
     ymax, xmax = map.shape
     seen_nodes = [None] * (ymax * xmax)
     to_be_processed_lookup: list[Any] = [None] * (ymax * xmax)
@@ -408,9 +574,7 @@ def find_path_astar(map, start: tuple, goal: tuple) -> list[tuple]:
     gx, gy = goal
 
     # Initialise heap
-    h = BinaryHeap()
-    h.satifies_heap_invariant = satisfies_fcost_invariant       # override internal heap-invariant comparison
-    h.get_best_child = get_lowest_fcost_child                   # override child comparison (for .pop())
+    h = MyAStarMinHeap()
 
     h.push(start_node)
 
@@ -644,8 +808,8 @@ if __name__ == '__main__':
     START = (90, 65)
     GOAL = (83, 75)
 
-    PLOT_RESULTS = True
-    RUN_PROFILING = False
+    PLOT_RESULTS = False
+    RUN_PROFILING = True
     RUN_TESTS = False
     ################### USER CONFIG END ###################
 
