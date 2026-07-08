2f96734f103dc0133e6b3a04301ecdd434d70818
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-07-09 00:18:48 +1000

    chore: release-process numbering

--- multiplay/skirmish/FishBot.js
+++ multiplay/skirmish/FishBot.js
@@ -69,13 +69,13 @@ const DEBUG_MODE_ON = false;
 8. Update `README.md` with summary of changes.
 9. Update `CHANGELOG.md`.
 10. Commit all changes as the latest commit on the `vx.y.z-development` branch.
-10. Open a PR on GitHub (titled 'FishBot vx.y.z Release') & merge into `main`.
-11. On the main branch, add tag: `fishbot-vx.y.z` and push to origin.
-12. .zip the completed mod files as: `fishbot-vx.y.z/multiplay/skirmish/[bot-files-here]` and move this .zip file to the `.\releases` folder.
-13. Create a new "Release" on GitHub titled "FishBot vx.y.z". 
-	13a. Set the release tag to the tag created two steps before.
-	13b. Copy-paste the `CHANGELOG.md` description for the new release into the "Release notes" section.
-	13c. Attach the .zip file from Step 12 into the field labeled: "Attach binaries by dropping them here or selecting them". 
+11. Open a PR on GitHub (titled 'FishBot vx.y.z Release') & merge into `main`.
+12. On the main branch, add tag: `fishbot-vx.y.z` and push to origin.
+13. .zip the completed mod files as: `fishbot-vx.y.z/multiplay/skirmish/[bot-files-here]` and move this .zip file to the `.\releases` folder.
+14. Create a new "Release" on GitHub titled "FishBot vx.y.z". 
+	a. Set the release tag to the tag created two steps before.
+	b. Copy-paste the `CHANGELOG.md` description for the new release into the "Release notes" section.
+	c. Attach the .zip file from Step 12 into the field labeled: "Attach binaries by dropping them here or selecting them". 
 */
 
 ////////////////////////////////////////////////////////////////////////////////////////////
