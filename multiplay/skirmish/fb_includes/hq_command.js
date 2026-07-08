1a3c147971ba04e742a79607cc1001a77586040f
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-07-08 18:43:15 +1000

    reduced number of repair to 1, rebalanced production weights to reflect

--- multiplay/skirmish/fb_includes/hq_command.js
+++ multiplay/skirmish/fb_includes/hq_command.js
@@ -56,7 +56,7 @@ class CommandCenter {
 			'MAX_ADA': 3,
 			'MAX_SENSOR': 1,
 			'MAX_INFANTRY': 6,
-			'MAX_REPAIR': 3,
+			'MAX_REPAIR': 1,
 		}
 		this.TOTAL_UNITS_PER_BRIGADE = Object.values(this.FISHBOT_BRIGADE_COMPOSITION).reduce((a, b) => a + b, 0);
 
@@ -894,11 +894,11 @@ class CommandCenter {
 		if (SUFFICIENT_CAVALRY) {
 			w_strategic = {
 				'heavyCavalry': 1,
-				'lightCavalry': 1,
+				'lightCavalry': 0.95,
 				'shortRangeArtillery': 1,
 				'ADA': 0.9,
 				'sensor': 0.2,
-				'repair': 0.9,
+				'repair': 0.85,
 			};
 		}
 
