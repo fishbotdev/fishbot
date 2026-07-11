2adb0d98c9dc4f130ecbc98ac8b96878d21187f1
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-07-11 20:34:47 +1000

    feat: optionally writes directly to the dev/maps library (resolves username)

--- python_helper_scripts/bot_test_map_packager/run_test_map_packager.py
+++ python_helper_scripts/bot_test_map_packager/run_test_map_packager.py
@@ -90,7 +90,10 @@ if __name__ == "__main__":
 
     SOURCE_DIRECTORY_NAME = 'v4.7.0_base_maps'
     source_directory = Path.cwd() / SOURCE_DIRECTORY_NAME
-    output_directory = Path.cwd() / f'custom_{SOURCE_DIRECTORY_NAME}'
+    # output_directory = Path.cwd() / f'custom_{SOURCE_DIRECTORY_NAME}'
+
+    # Write directly to the dev/maps library
+    output_directory = path = Path("~/OneDrive/Documents/wz2100_config_dir/maps").expanduser()
 
     batch_report = run_batch_map_packaging(source_directory, output_directory)
 
