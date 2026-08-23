27782d2f9205057da8c40971fab6907b10e57bac
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-08-23 15:51:07 +1000

    fix: increased direct fire radius to 10 to reduce oscillations on rush

--- multiplay/skirmish/fb_includes_v0_5_2/hq_command.js
+++ multiplay/skirmish/fb_includes_v0_5_2/hq_command.js
@@ -54,8 +54,8 @@ class CommandCenter {
 
 		/** @type {GroundForceParameters} */
 		this.GROUND_FORCE_PARAMETERS = {
-			IMMEDIATE_DIRECT_FIRE_RADIUS: 8,
-			EFFECTIVE_FIRE_SUPPORT_RADIUS: 9,		// todo: this should be adaptive - when the brigade has a sensor, this is better, without, it is restricted by sight range of the front units
+			IMMEDIATE_DIRECT_FIRE_RADIUS: 10,
+			EFFECTIVE_FIRE_SUPPORT_RADIUS: 10,		// todo: this should be adaptive - when the brigade has a sensor, this is better, without, it is restricted by sight range of the front units
 			EFFECTIVE_ADA_RADIUS: 12,
 		};
 
