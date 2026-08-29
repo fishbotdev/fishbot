547c743c353765a2c06e4367bc30defecf19d436
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-08-29 13:48:30 +1000

    docs: updated LOC stats

--- multiplay/skirmish/FishBot_v0_5_2.js
+++ multiplay/skirmish/FishBot_v0_5_2.js
@@ -42,6 +42,7 @@
 	- 5330 JS @ 10 Jul 2026: v0.4.2 release (commit `3781360`)
 	- 5315 JS @ 29 Jul 2026: v0.5.0 release 
 	- 5580 JS @ 04 Aug 2026: v0.5.1 release
+	- 5626 JS @ 29 Aug 2026: v0.5.2 release
 */
 
 
