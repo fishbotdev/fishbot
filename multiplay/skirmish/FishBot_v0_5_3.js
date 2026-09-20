9cf1b2e0e2ed1160a5c93954372f221ad1cd24fc
fishbotdev <89434460+fishbotdev@users.noreply.github.com>
2026-09-20 12:43:07 +1000

    chore: v0.5.3 release (#125)
    * chore: set DEBUG_MODE_ON = false for release
    
    * docs: updated LOC stats

--- multiplay/skirmish/FishBot_v0_5_3.js
+++ multiplay/skirmish/FishBot_v0_5_3.js
@@ -41,6 +41,7 @@
 	- 5315 JS @ 29 Jul 2026: v0.5.0 release 
 	- 5580 JS @ 04 Aug 2026: v0.5.1 release
 	- 5718 JS @ 01 Sep 2026: v0.5.2 release
+	- 5864 JS @ 20 Sep 2026: v0.5.3 release
 */
 
 const FISHBOT_VERSION = "0.5.3";
@@ -54,7 +55,7 @@ const FB_INCLUDES = FISHBOT_PATH + "fb_includes_v0_5_3/";
 //	 - Show some useful debug information in the console
 //	 - Automatically colour players 0, 1, 2
 //	 - Transform Player 0 (forced human player slot) to spectator mode (used for automated bot testing)
-const DEBUG_MODE_ON = true;
+const DEBUG_MODE_ON = false;
 
 // For developers, please refer to `docs/DEVELOPMENT.md` for important info e.g. release checklist.
 
